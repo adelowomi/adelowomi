a66ce3236e087859489ed4dd33cf83b3e53503d6
adelowomi <adelowomi@gmail.com>
2025-09-11 01:35:39 +0100

    updated contact information

--- src/components/shared/Contact.tsx
+++ src/components/shared/Contact.tsx
@@ -5,30 +5,30 @@ import { SendIcon, PhoneIcon, EmailIcon, AddressIcon } from "@/icons";
 
 const Contact = () => {
   return (
-    <div className="flex flex-col lg:flex-row gap-10 lg:gap-20 px-6 sm:px-12 lg:px-28 py-12 lg:py-24">
+    <div className="flex flex-col gap-10 px-6 py-12 lg:flex-row lg:gap-20 sm:px-12 lg:px-28 lg:py-24">
       <div className="w-full lg:w-[500px]">
-        <h1 className="w-full font-semibold text-2xl sm:text-3xl uppercase text-primary font-besley pb-2">
+        <h1 className="w-full pb-2 text-2xl font-semibold uppercase sm:text-3xl text-primary font-besley">
           Let's Build Something Amazing Together
         </h1>
         <hr className="horizontal-line" />
 
-        <div className="mt-10 lg:mt-20 flex flex-col gap-y-4">
-          <div className="flex items-center gap-4 text-primary font-normal text-lg lg:text-xl">
-            <span className="w-10 h-10 lg:w-12 lg:h-12 p-2 flex items-center justify-center rounded-full purple-gradient font-archivo">
+        <div className="flex flex-col mt-10 lg:mt-20 gap-y-4">
+          <div className="flex items-center gap-4 text-lg font-normal text-primary lg:text-xl">
+            <span className="flex items-center justify-center w-10 h-10 p-2 rounded-full lg:w-12 lg:h-12 purple-gradient font-archivo">
               <PhoneIcon />
             </span>
-            +2340867547854
+            +2348180041801
           </div>
 
-          <div className="flex items-center gap-4 text-primary font-normal text-lg lg:text-xl">
-            <span className="w-10 h-10 lg:w-12 lg:h-12 p-2 flex items-center justify-center rounded-full purple-gradient font-archivo">
+          <div className="flex items-center gap-4 text-lg font-normal text-primary lg:text-xl">
+            <span className="flex items-center justify-center w-10 h-10 p-2 rounded-full lg:w-12 lg:h-12 purple-gradient font-archivo">
               <EmailIcon />
             </span>
-            Abcdefg@gmail.com
+            me@adelowomi.com
           </div>
 
-          <div className="flex items-center gap-4 text-primary font-normal text-lg lg:text-xl">
-            <span className="w-10 h-10 lg:w-12 lg:h-12 p-2 flex items-center justify-center rounded-full purple-gradient font-archivo">
+          <div className="flex items-center gap-4 text-lg font-normal text-primary lg:text-xl">
+            <span className="flex items-center justify-center w-10 h-10 p-2 rounded-full lg:w-12 lg:h-12 purple-gradient font-archivo">
               <AddressIcon />
             </span>
             Lagos, Nigeria
@@ -39,7 +39,7 @@ const Contact = () => {
       <div className="inline-flex w-full lg:w-[630px] items-center rounded-lg border-[0.5px] border-solid border-secondary/30 bg-gradient-to-br from-secondary/5 to-accent/10 px-6 sm:px-8 lg:px-12 pt-8 lg:pt-12 pb-12 lg:pb-20">
         <form className="w-full">
           <div className="flex flex-col mb-4">
-            <label className="text-base lg:text-lg font-medium font-archivo text-primary">
+            <label className="text-base font-medium lg:text-lg font-archivo text-primary">
               Name
             </label>
             <input
@@ -50,7 +50,7 @@ const Contact = () => {
           </div>
 
           <div className="flex flex-col mb-4">
-            <label className="text-base lg:text-lg font-medium font-archivo text-primary">
+            <label className="text-base font-medium lg:text-lg font-archivo text-primary">
               Email
             </label>
             <input
@@ -61,7 +61,7 @@ const Contact = () => {
           </div>
 
           <div className="flex flex-col">
-            <label className="text-base lg:text-lg font-medium font-archivo text-primary">
+            <label className="text-base font-medium lg:text-lg font-archivo text-primary">
               Message
             </label>
             <textarea
